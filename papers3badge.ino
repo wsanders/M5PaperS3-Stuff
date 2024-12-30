5871c1c1e3cb7ee9f28793a915a084de599262d8
W Sanders <wsanders@wsanders.net>
2024-12-29 18:32:14 -0800

    Add files via upload

--- papers3badge.ino
+++ papers3badge.ino
@@ -1,12 +1,11 @@
 // This version uses drawXBitmap which luckily works because reverse
-// engineering XBM file format was tedious. Create your badge in GIMP, output
+// engineering XBM file format was tedious. Edit badge in GIMP, output
 // with XBM file format, tweak macro names to not start with number.
 // gcc header files seem to need to end with .h.
 
-// After the program starts, a dot in the lower right of the scren will blink
-// to let you know the device is powered on. While the dot is TFT_WHITE, 
-// double click the power button to power it off.
+// ToDo: RTC, date display 
 
+#include <epdiy.h>
 #include <M5Unified.h>
 #include "IMG_0573.h"
 
@@ -18,23 +17,33 @@ int color = 0;
 void setup(void)
 {
   auto cfg = M5.config();
-  cfg.serial_baudrate = 115200;
   M5.begin(cfg);
-
   M5.Display.setTextSize(5.0);
   if (M5.Display.isEPD())
   {
-    M5.Display.setEpdMode(epd_mode_t::epd_fastest);
+    M5.Display.setEpdMode(epd_mode_t::epd_quality);
   }
   // eyelet at top
   M5.Display.setRotation(M5.Display.getRotation() ^ 2);
-  // For later - try tone, etc.
-  //M5.Speaker.setVolume(255);
-  //M5.Speaker.tone(2000, 100); 
+  // Crashes here M5.begin();
+  M5.Speaker.setVolume(255);
+  M5.Speaker.tone(2000, 100); 
   M5.Display.println("I RUN");
   
   M5.Display.startWrite();
   M5.Display.fillScreen(TFT_WHITE);
+  sleep(1);
+  M5.Display.endWrite();
+  sleep(1);
+  M5.Display.startWrite();
+  M5.Display.fillScreen(TFT_BLACK);
+  M5.Display.endWrite();
+  sleep(1);
+  M5.Display.startWrite();
+  M5.Display.fillScreen(TFT_WHITE);
+  M5.Display.endWrite();
+  sleep(1);
+  M5.Display.startWrite();
   M5.Display.drawXBitmap(0, 0, xbm_bits, xbm_width, xbm_height, TFT_BLACK);
   M5.Display.endWrite();
 }
