0d5e9e5b0b25433c2a9c654020d2138b2068204e
W Sanders <wsanders@wsanders.net>
2024-12-30 13:52:12 -0800

    New features from M5United
    - M5.Imu.sleep call hopefully saves some power - hard to tell if it's working.
    - Draw local time in PST8PDT timezone at bottom of screen. RTC must be previously set to UTC.
    - Use M5.Power.timerSleep to sleep device and then wake it up every hour to update the date.

--- papers3badge.ino
+++ papers3badge.ino
@@ -12,29 +12,25 @@
 int pixel[3];
 char *data;
 int color = 0;
-
+int w;
+static constexpr const char* const wd[7] = {"Sun","Mon","Tue","Wed","Thr","Fri","Sat"};
+// Dont forget the null!
+char datestring[] = "XXX MM/YY/DDDD                          ";
 
 void setup(void)
 {
   auto cfg = M5.config();
   M5.begin(cfg);
-  M5.Display.setTextSize(5.0);
+  M5.Imu.sleep();
+  M5.Display.setTextSize(4.0);
   if (M5.Display.isEPD())
   {
     M5.Display.setEpdMode(epd_mode_t::epd_quality);
   }
   // eyelet at top
   M5.Display.setRotation(M5.Display.getRotation() ^ 2);
-  // Crashes here M5.begin();
   M5.Speaker.setVolume(255);
-  M5.Speaker.tone(2000, 100); 
-  M5.Display.println("I RUN");
-  
-  M5.Display.startWrite();
-  M5.Display.fillScreen(TFT_WHITE);
-  sleep(1);
-  M5.Display.endWrite();
-  sleep(1);
+  M5.Speaker.tone(1000, 1000); 
   M5.Display.startWrite();
   M5.Display.fillScreen(TFT_BLACK);
   M5.Display.endWrite();
@@ -45,10 +41,19 @@ void setup(void)
   sleep(1);
   M5.Display.startWrite();
   M5.Display.drawXBitmap(0, 0, xbm_bits, xbm_width, xbm_height, TFT_BLACK);
-  M5.Display.endWrite();
-  //'class m5::M5Unified' has no member named 'shutdown'
-  //M5.shutdown();
 
+  // Print the date
+  auto t = time(nullptr);
+  setenv("TZ","PST8PDT",1);
+  tzset();
+  auto tm = localtime(&t);
+  sprintf(datestring, "%3s %02d/%02d/%04d",
+      wd[tm->tm_wday], tm->tm_mon+1, tm->tm_mday, tm->tm_year+1900);
+  M5.Display.setFont(&fonts::DejaVu12);
+  w = M5.Display.textWidth(datestring);
+  M5.Display.drawString(datestring,(540-w)/2,900);
+  M5.Display.endWrite();
+  M5.Power.timerSleep(7200);
 }
 
 void loop(void)
